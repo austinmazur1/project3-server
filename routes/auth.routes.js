a453351ddc2b329f91fd5d906bbcfbaba69e70a4
Austin Mazur <mazuraustin1@gmail.com>
2023-07-01 09:46:03 +0200

    Add seller and message model, connect DB

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,8 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Require the User model in order to interact with the database
-const User = require("../models/User.model");
+// const User = require("../models/User.model");
+const Seller = require("../models/Seller.model");
 
 // Require necessary (isAuthenticated) middleware in order to control access to specific routes
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
@@ -18,7 +19,7 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
-  const { email, password, name } = req.body;
+  const { email, password, name, seller } = req.body;
 
   // Check if email or password or name are provided as empty strings
   if (email === "" || password === "" || name === "") {
@@ -44,7 +45,7 @@ router.post("/signup", (req, res, next) => {
   }
 
   // Check the users collection if a user with the same email already exists
-  User.findOne({ email })
+  Seller.findOne({ email })
     .then((foundUser) => {
       // If the user with the same email already exists, send an error response
       if (foundUser) {
@@ -58,15 +59,15 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return Seller.create({ email, password: hashedPassword, name, seller });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, name, _id, seller } = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email, name, _id, seller };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
@@ -85,7 +86,7 @@ router.post("/login", (req, res, next) => {
   }
 
   // Check the users collection if a user with the same email exists
-  User.findOne({ email })
+  Seller.findOne({ email })
     .then((foundUser) => {
       if (!foundUser) {
         // If the user is not found, send an error response
