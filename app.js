c466aa62790447a2f197dc974f8bcf9ebd0e50b9
Austin Mazur <mazuraustin1@gmail.com>
2023-07-01 11:48:00 +0200

    Update app.js file and the routes

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
-const authRoutes = require("./routes/auth.routes");
+// Do the same for the buyer auth routes
+const authRoutes = require("./routes/auth.seller.routes");
 app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
