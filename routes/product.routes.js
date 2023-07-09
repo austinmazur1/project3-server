679239546e97fb4898c5e8a2698261c2e5183709
Austin Mazur <mazuraustin1@gmail.com>
2023-07-09 20:03:27 +0200

    Ability to add photo on product creation

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,16 +2,27 @@ const express = require("express");
 const router = express.Router();
 // const mongoose = require("mongoose");
 const Product = require("../models/Product.model");
+const multer = require("multer");
+
+const upload = multer()
 
 //  POST /api/projects  -  Creates a new product
-router.post("/seller/new-product", (req, res, next) => {
-  const { productName, description, startingPrice, duration, images } =
+router.post("/seller/new-product", upload.single("image"), (req, res, next) => {
+  const { productName, description, startingPrice, duration } =
     req.body;
+    const image = req.file;
   console.log(req.body);
+  console.log(image)
 
-  Product.create({ productName, description, startingPrice, duration, images })
-    .then((response) => res.json(response))
+  Product.create({ productName, description, startingPrice, duration, image: {
+    data: image.buffer,
+    contentType: image.mimetype
+  } })
+    .then((response) => {
+      res.json(response)
+    })
     .catch((err) => res.json(err));
 });
 
 module.exports = router;
+
