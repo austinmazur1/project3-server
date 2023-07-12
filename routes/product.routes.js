63ddc7d1f538a23bfd0d570a8635dcd0a0243453
Austin Mazur <mazuraustin1@gmail.com>
2023-07-12 20:43:14 +0200

    User id now linked with product

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,25 +4,37 @@ const router = express.Router();
 const Product = require("../models/Product.model");
 const multer = require("multer");
 
-const upload = multer()
+const upload = multer();
+
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 //  POST /api/projects  -  Creates a new product
-router.post("/seller/new-product", upload.single("image"), (req, res, next) => {
-  const { productName, description, startingPrice, duration } =
-    req.body;
+router.post(
+  "/seller/new-product",
+  isAuthenticated,
+  upload.single("image"),
+  (req, res, next) => {
+    const { productName, description, startingPrice, duration } = req.body;
     const image = req.file;
-  console.log(req.body);
-  console.log(image)
+    const userId = req.payload._id;
+    // console.log("PAYLOAD", req.payload);
 
-  Product.create({ productName, description, startingPrice, duration, image: {
-    data: image.buffer,
-    contentType: image.mimetype
-  } })
-    .then((response) => {
-      res.json(response)
+    Product.create({
+      productName,
+      description,
+      startingPrice,
+      duration,
+      seller: userId,
+      image: {
+        data: image.buffer,
+        contentType: image.mimetype,
+      },
     })
-    .catch((err) => res.json(err));
-});
+      .then((response) => {
+        res.json(response);
+      })
+      .catch((err) => res.json(err));
+  }
+);
 
 module.exports = router;
-
