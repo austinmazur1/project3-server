bf92e0e5137713279760e3f16070e405b1efa14e
Austin Mazur <mazuraustin1@gmail.com>
2023-07-29 09:04:42 +0200

    Try setting up backend

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Product = require('../models/Product.model');
+const Buyer = require('../models/Buyer.model')
+const Seller = require('../models/Seller.model')
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get("/profile/:id", async (req, res, next) => {
+  const id = req.params.id
+  try {
+    const buyer = await Buyer.findById(id);
+    if(buyer) {
+      return res.json({user: buyer, userType: 'buyer'});
+    }
+
+      const seller = await Seller.findById(id);
+      if(seller) {
+        return res.json({user: seller, userType: 'seller'});
+      }
+
+      res.status(404).json({error: 'User not found'});
+  } catch (error) {
+    res.status(500).json({error: 'Internal server error'})
+  }
 });
 
 router.get('/seller', (req, res, next) => {
